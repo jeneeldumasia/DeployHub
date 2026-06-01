cc642f05e6fbe6c26846f1c85a0c2226360441b8
Jeneel-Itara <jeneel.dumasia@itara.io>
2026-06-01 18:24:31 +0530

    fix: pin helm provider version to resolve syntax error

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
